4a6d6964d02a93f1283a600b50d6393377796a80
vvip <vvip@mail.ru>
2019-10-25 07:33:30 +0700

    Вывод в монитор порта версии прошивки при запуске

--- firmware/GyverString_v1.00/GyverString_v1.00.ino
+++ firmware/GyverString_v1.00/GyverString_v1.00.ino
@@ -181,6 +181,8 @@ void setup() {
   Serial.begin(115200);
   delay(10);
 
+  Serial.println(F("\n\nWiFi String v.1.00.2019.1025"));
+
   // Инициализация EEPROM и загрузка сохраненных параметров
   EEPROM.begin(512);
   loadSettings();
