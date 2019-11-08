7f17bfbc6e5be3802b4b9a024f22b33bc3bb0c84
vvip <vvip@mail.ru>
2019-11-08 15:46:09 +0700

    Обновление версии

--- firmware/GyverString_v1.00/GyverString_v1.00.ino
+++ firmware/GyverString_v1.00/GyverString_v1.00.ino
@@ -6,7 +6,7 @@
 // Дальнейшее развитие: vvip, 2019
 // https://AlexGyver.ru/
 
-#define FIRMWARE_VER F("\n\nGyverString-WiFi v.1.00.2019.1028")
+#define FIRMWARE_VER F("\n\nGyverString-WiFi v.1.00.2019.1108")
 
 #define FASTLED_INTERRUPT_RETRY_COUNT 0
 #define FASTLED_ALLOW_INTERRUPTS 0
